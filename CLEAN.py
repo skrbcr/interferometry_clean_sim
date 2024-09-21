d6c1141e1b3dfbeaf308b989b29e4400f6fb82a1
skrbcr <90087433+skrbcr@users.noreply.github.com>
2024-09-21 20:59:17 +0900

    Create CLEAN.py

---
+++ CLEAN.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class CLEAN:
+    def __init__(self):
+        pass
+
+    # Setup antenna array and show their positions, uv coverage and the synthesized beam
+
+    # Load image file and construct dirty image
+
+    # Implement the CLEAN algorithm
+
