b75587faa2ad94b36613951ea48577293a7e62e3
skrbcr <90087433+skrbcr@users.noreply.github.com>
2024-09-21 21:10:34 +0900

    CLEAN.py: add function name

--- CLEAN.py
+++ CLEAN.py
@@ -1,12 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
 
 class CLEAN:
     def __init__(self):
         pass
 
     # Setup antenna array and show their positions, uv coverage and the synthesized beam
+    def set_antenna_array(self):
+        pass
 
     # Load image file and construct dirty image
+    def create_dirty_image(self):
+        pass
 
     # Implement the CLEAN algorithm
+    def clean(self):
+        pass
 
